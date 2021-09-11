a3a3a215a0494415f29010b2239faa83b215b631
misitebao <i@misitebao.com>
2021-09-11 19:19:14 +0800

    feat(website): add website

---
+++ website/docusaurus.config.js
@@ -0,0 +1,135 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'Awesome Wails',
+  tagline: 'A wails template based on Vue and Vue-Router',
+  url: 'https://awesome-wails.vercel.app',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'misitebao', // Usually your GitHub org/user name.
+  projectName: 'awesome-wails', // Usually your repo name.
+  // i18n
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh-Hans'],
+    localeConfigs: {
+      en: {
+        label: 'English',
+        direction: 'ltr',
+      },
+      "zh-Hans": {
+        label: '简体中文',
+        direction: 'ltr',
+      },
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/misitebao/awesome-wails/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/misitebao/awesome-wails/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Awesome Wails',
+        logo: {
+          alt: 'Awesome Wails Site Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          // { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+            dropdownItemsAfter: [
+              { to: 'https://github.com/misitebao/awesome-wails', label: 'Help Translate 💖', }, //Can add custom pages
+            ],
+          },
+          {
+            href: 'https://github.com/misitebao/awesome-wails',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: 'Community',
+          //   items: [
+          //     {
+          //       label: 'Stack Overflow',
+          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+          //     },
+          //     {
+          //       label: 'Discord',
+          //       href: 'https://discordapp.com/invite/docusaurus',
+          //     },
+          //     {
+          //       label: 'Twitter',
+          //       href: 'https://twitter.com/docusaurus',
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: 'More',
+          //   items: [
+          //     {
+          //       label: 'Blog',
+          //       to: '/blog',
+          //     },
+          //     {
+          //       label: 'GitHub',
+          //       href: 'https://github.com/facebook/docusaurus',
+          //     },
+          //   ],
+          // },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Misitebao, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
---
+++ website/src/pages/index.md
@@ -0,0 +1,19 @@
+<p align="center">
+   <img src="/img/logo.png" width="180" height="180"/><br/>
+</p>
+<p align="center">
+List of go framework software libraries
+</p>
+<p align="center">
+  <a href="https://github.com/misitebao/awesome-wails/blob/main/LICENSE"><img alt="GitHub" src="https://img.shields.io/github/license/misitebao/awesome-wails?style=flat-square"/></a>
+  <a href="https://github.com/misitebao/standard-repository"><img alt="GitHub" src="https://img.shields.io/badge/Readme--Style-standard--repository-brightgreen?style=flat-square&color=f83500"/></a>
+  <a href="https://github.com/misitebao/awesome-wails"><img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/misitebao/awesome-wails?style=flat-square"/></a>
+  <a href="https://github.com/misitebao/awesome-wails/issues"><img alt="GitHub issues" src="https://img.shields.io/github/issues/misitebao/awesome-wails?style=flat-square"/></a>
+  <a href="https://github.com/misitebao/awesome-wails/pulls"><img alt="GitHub issues" src=" https://img.shields.io/github/issues-pr/misitebao/awesome-wails?style=flat-square"/></a>
+</p>
+
+## Template
+
+- [wails-template-vue](https://github.com/misitebao/wails-template-vue) - A wails template based on Vue and Vue-Router
+
+## Project
